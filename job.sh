07259ec389d89f42d1ae23996738b9d95ea5c711
jliellen <43282021+jliellen@users.noreply.github.com>
2023-04-29 19:20:04 -0400

    Update job.sh

--- job.sh
+++ job.sh
@@ -2,9 +2,5 @@
 
 for i in {6..10};
 do
-    python3 preprocess/geolife.py --res "$i"
-    python3 preprocess/rome.py --res "$i"
     python3 preprocess/porto.py --res "$i"
-    python3 preprocess/tdrive.py --res "$i"
-    python3 preprocess/preprocess.py --res "$i"
 done
