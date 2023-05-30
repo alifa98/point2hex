47f0fdcc338451fc794a02bd066ba4895731c977
Ali Faraji <billa.svpc@gmail.com>
2023-05-30 10:06:29 -0400

    docs & style: add documentation for loc2point code

--- loc2point_run.py
+++ loc2point_run.py
@@ -1,13 +1,13 @@
-# You can run this file to generate points from locations.
-# this file uses OSRM API to generate routes between locations and then
-# generate points on the route.
+"""
+This module is used to generate points from locations using the OSRM API. It generates routes between locations and then
+generates points on the route.
 
-# the input file should be a csv file with the following columns:
-# start_point_lon, start_point_lat, end_point_lon, end_point_lat
-# the output file will be a csv file with the following columns:
-# start_point_lon, start_point_lat, end_point_lon, end_point_lat, route_points
-# route_points is a list of points (tuple) on the route between start and end points.
-# if the split option is enabled, the output file will be multiple csv files with the same columns for each thread.
+The input should be a csv file with the following columns: start_point_lon, start_point_lat, end_point_lon, end_point_lat.
+The output will be a csv file with the following columns: start_point_lon, start_point_lat, end_point_lon, end_point_lat, route_points.
+route_points is a list of points (tuple) on the route between start and end points.
+
+If the split option is enabled, the output will be multiple csv files with the same columns for each thread.
+"""
 
 import pandas as pd
 from lib.GetPointsThread import GeneratePointsThread
@@ -15,24 +15,29 @@ from lib.Utils import get_logger
 from lib.api.OpenSteetMap import OpenStreetAPI
 from lib.ArgsParse import parse_args
 
-
 def get_split_points(total, num_threads):
+    """
+    Returns a list of points where to split the data for threading.
+
+    :param total: Total number of data points
+    :param num_threads: Number of threads to be created
+    :returns: A list of split points of the list to split the data for threading
+    """
     step = (total // int(num_threads)) + 1
     split_points_list = [i for i in range(0, total, step)]
 
-    # make sure all of the instaces included (last step may contain more values)
+    # Make sure all of the instances included (last step may contain more values)
     split_points_list.pop()
-    # rages are like [start,end) so we do not need to add -1 here.
+    # Ranges are like [start,end) so we do not need to add -1 here.
     split_points_list.append(total)
 
     return split_points_list
-
 
 if __name__ == "__main__":
     args = parse_args()
     logger = get_logger("loc2point")
 
-    # the API that we want to use
+    # Initialize the OpenStreetAPI
     api = OpenStreetAPI(args.base_url)
 
     # Loading Data file
@@ -40,13 +45,13 @@ if __name__ == "__main__":
     data_file = pd.read_csv(args.input_file)
 
     try:
-        # split points of the data to threads
+        # Split points of the data to threads
         split_points = get_split_points(len(data_file), args.threads)
 
         threads_list = []
 
-        logger.info("Creating threads...")
         # Create new threads
+        logger.info("Creating threads...")
         for i in range(len(split_points) - 1):
             threads_list.append(
                 GeneratePointsThread(
@@ -60,15 +65,15 @@ if __name__ == "__main__":
                 )
             )
 
-        # Starting the threads
+        # Start the threads
         logger.info("Starting...")
-        for t in threads_list:
-            t.start()
+        for thread in threads_list:
+            thread.start()
 
-        # waiting for threads
+        # Wait for all threads to finish
         logger.info("Processing...")
-        for t in threads_list:
-            t.join()
+        for thread in threads_list:
+            thread.join()
 
         logger.debug("All threads are finished.")
         logger.info("Job is done.")
