ca1547ba7126d769a20f886afa834f5152581a39
Ali Faraji <billa.svpc@gmail.com>
2023-05-31 08:40:03 -0400

    feat: add plot_run.py command line to visualize the generated hexagon datasets

---
+++ plot_run.py
@@ -0,0 +1,21 @@
+import argparse
+import os
+import pandas as pd
+from lib.viz import Seqviz
+
+parser = argparse.ArgumentParser(
+    description='Generate hexagon heatmap from trajectory data')
+
+parser.add_argument('input_file', help='Input file path')
+parser.add_argument('-z', '--zoom', type=int, default=12,
+                    help='The zoom level of the map')
+args = parser.parse_args()
+
+
+#creat the plot directly from the hex sequence
+os.makedirs('out/plots', exist_ok=True)
+
+traj_seq = pd.read_csv(args.input_file)
+viz_seq = Seqviz(traj_seq['higher_order_trajectory'].tolist(), 'heatmap')
+# viz_seq = Seqviz(traj_seq['higher_order_trajectory'][1], 'seq')
+viz_seq.show_map(args.zoom)
