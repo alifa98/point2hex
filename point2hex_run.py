3c2a0645220e580ef322f4846e3872630d3696d0
Ali Faraji <billa.svpc@gmail.com>
2023-06-13 11:08:59 -0400

    fix: remove running by python features for cocurrency because it makes problem, new multithread approach will be implemented soon.

--- point2hex_run.py
+++ point2hex_run.py
@@ -1,8 +1,8 @@
-import concurrent.futures
 import os
 import argparse
 import pandas as pd
 from lib.traj2h3 import Points2h3
+
 
 def main(resolutions_list, input_dir, output_dir, column_name):
 
@@ -12,36 +12,29 @@ def main(resolutions_list, input_dir, output_dir, column_name):
         for root, dirs, files in os.walk(direc):
             for name in files:
                 full_files.append(os.path.join(root, name))
-                
+
         return full_files
 
-    files_path_list = get_files(input_dir)  
+    files_path_list = get_files(input_dir)
     print("Reading in the .csv files...")
 
     # Read datasets from trajectory route points
     data_list = []
-    for index, file_path in enumerate(files_path_list):
+    for file_path in files_path_list:
         data_list.append(pd.read_csv(file_path, encoding='latin1'))
 
     dataframe = pd.concat(data_list, ignore_index=True)
 
     # Drop columns that won't be used
     dataframe.dropna(inplace=True, subset=[column_name])
-    
-    print ("Size of data frame: ", dataframe.shape)
-    
+
+    print("Size of data frame: ", dataframe.shape)
+
     dataset_processor = Points2h3(dataframe, output_dir, column_name)
     dataset_processor.pre_process()
 
-    threads_list = []
-
-    with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
-        future_to_res = {executor.submit(dataset_processor.get_hexseq, res): res for res in resolutions_list}
-        for future in concurrent.futures.as_completed(future_to_res):
-            try:
-                print("the resolution ", future_to_res[future] ," is finished.")
-            except Exception as exc:
-                print("resolution ", future_to_res[future], " has generated an expecption, ",exc)
+    for res in resolutions_list:
+        dataset_processor.get_hexseq(res)
 
 
 if __name__ == '__main__':
@@ -53,12 +46,14 @@ if __name__ == '__main__':
     # Define the command-line arguments
     parser.add_argument('input_dir', help='Input files directory')
     parser.add_argument('-o', "--output", help='Output directory path (output file name is output_resX.csv)',
-                    action='store', default='./')
-    parser.add_argument('-r','--resoluton', type=str, default="6", help='The list of resolutions for generating hexagons')
+                        action='store', default='./')
+    parser.add_argument('-r', '--resoluton', type=str, default="6",
+                        help='The list of resolutions for generating hexagons')
     parser.add_argument(
-    '-c', "--column", help='Route points column name', default='route_points')
+        '-c', "--column", help='Route points column name', default='route_points')
     args = parser.parse_args()
 
-    resolutions_list = [int(res.strip()) for res in args.resoluton.strip().split(",") ]
+    resolutions_list = [int(res.strip())
+                        for res in args.resoluton.strip().split(",")]
 
     main(resolutions_list, args.input_dir, args.output, args.column)
